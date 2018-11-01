facafbf329b81657b69f6d3961fb9480202d2f39
Andrew DeRusha <acderusha@wpi.edu>
2018-11-01 14:05:55 +0100

    added map

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ var server = http.createServer (function (req, res) {
         if(req.url === "/"){
             sendFile(res, 'public/html/home.html')
         }
+        else if(req.url === "/css/home.css") {
+            sendFile(res,'public/css/home.css', 'text/css')
+        }
         else{
             sendFile(res, 'public/index_404.html')
         }
